ef7051c7d6ce563a51a17da6c76fcde5cee38cd8
Georgi Atanasov <gaterin98@gmail.com>
2020-04-06 00:02:32 +0200

    Updated SQL file

--- sql/zorgapp.sql
+++ sql/zorgapp.sql
@@ -1,32 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 05, 2020 at 11:48 PM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `zorgapp`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `drug`
---
+CREATE DATABASE IF NOT EXISTS `zorgapp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `zorgapp`;
 
 CREATE TABLE `drug` (
   `id` int(11) NOT NULL,
@@ -36,19 +18,10 @@ CREATE TABLE `drug` (
   `dosage` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `drug`
---
-
-INSERT INTO `drug` (`id`, `name`, `description`, `type`, `dosage`) VALUES
-(1, 'paracetamol', 'paracetamol description', 'pill', 'paracetamol dosage '),
-(2, 'aspirine', 'aspirine discription', 'powder', 'aspirine dosage');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `drugprescription`
---
+TRUNCATE TABLE `drug`;
+INSERT INTO `drug` VALUES(1, 'paracetamol', 'paracetamol description', 'pill', 'paracetamol dosage ');
+INSERT INTO `drug` VALUES(2, 'aspirine', 'aspirine discription', 'powder', 'aspirine dosage');
+INSERT INTO `drug` VALUES(3, 'vaseline', 'vaseline desc', 'gel', 'vaseline dosage');
 
 CREATE TABLE `drugprescription` (
   `drugId` int(11) NOT NULL,
@@ -58,20 +31,12 @@ CREATE TABLE `drugprescription` (
   `intakeTime` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `drugprescription`
---
-
-INSERT INTO `drugprescription` (`drugId`, `profileId`, `startDate`, `endDate`, `intakeTime`) VALUES
-(1, 1, '2020-02-26', '2020-03-06', '23:00:00'),
-(2, 1, '2020-02-27', '2020-03-06', '23:00:00'),
-(2, 2, '2020-02-27', '2020-02-28', '12:00:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `profile`
---
+TRUNCATE TABLE `drugprescription`;
+INSERT INTO `drugprescription` VALUES(1, 1, '2020-02-26', '2020-03-06', '19:00:00');
+INSERT INTO `drugprescription` VALUES(2, 1, '2020-02-27', '2020-03-06', '19:00:00');
+INSERT INTO `drugprescription` VALUES(2, 2, '2020-02-27', '2020-02-28', '12:00:00');
+INSERT INTO `drugprescription` VALUES(2, 3, '2020-03-16', '2020-03-18', '07:19:00');
+INSERT INTO `drugprescription` VALUES(3, 3, '2020-03-16', '2020-03-17', '07:00:00');
 
 CREATE TABLE `profile` (
   `id` int(9) NOT NULL,
@@ -85,140 +50,75 @@ CREATE TABLE `profile` (
   `password` varchar(24) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `profile`
---
-
-INSERT INTO `profile` (`id`, `name`, `surname`, `age`, `weight`, `length`, `roleId`, `username`, `password`) VALUES
-(1, 'Georgi', 'Atanasov', 21, 95.60, 1.87, 1, 'georgi', 'atanasov'),
-(2, 'Karel', 'de Kleine', 24, 85.80, 1.78, 2, 'karel', 'dekleine');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `role`
---
+TRUNCATE TABLE `profile`;
+INSERT INTO `profile` VALUES(1, 'Georgi', 'Atanasov', 21, 95.60, 1.87, 1, 'georgi', 'atanasov');
+INSERT INTO `profile` VALUES(2, 'Karel', 'de Kleine', 24, 85.80, 1.78, 2, 'karel', 'dekleine');
+INSERT INTO `profile` VALUES(3, 'Kimberlyn', 'Colenbrander', 20, 70.00, 1.80, 1, '', '');
 
 CREATE TABLE `role` (
   `id` int(9) NOT NULL,
   `name` varchar(24) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `role`
---
-
-INSERT INTO `role` (`id`, `name`) VALUES
-(1, 'zorgverlener'),
-(2, 'patient');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `weightregistration`
---
+TRUNCATE TABLE `role`;
+INSERT INTO `role` VALUES(1, 'zorgverlener');
+INSERT INTO `role` VALUES(2, 'patient');
 
 CREATE TABLE `weightregistration` (
   `id` int(9) NOT NULL,
   `profileId` int(9) NOT NULL,
   `date` date NOT NULL,
+  `time` time NOT NULL,
   `weight` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `weightregistration`
---
+TRUNCATE TABLE `weightregistration`;
+INSERT INTO `weightregistration` VALUES(3, 2, '2020-03-05', '16:00:00', 87.3);
+INSERT INTO `weightregistration` VALUES(5, 1, '2020-03-03', '19:00:00', 80.3);
+INSERT INTO `weightregistration` VALUES(7, 1, '2020-03-07', '19:00:00', 95);
+INSERT INTO `weightregistration` VALUES(8, 1, '2020-03-08', '19:00:00', 76);
+INSERT INTO `weightregistration` VALUES(9, 1, '2020-03-20', '19:00:00', 100.3);
 
-INSERT INTO `weightregistration` (`id`, `profileId`, `date`, `weight`) VALUES
-(1, 1, '2020-03-05', 95.6),
-(2, 1, '2020-03-04', 95.3),
-(3, 2, '2020-03-05', 87.3);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `drug`
---
 ALTER TABLE `drug`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `drugprescription`
---
 ALTER TABLE `drugprescription`
   ADD PRIMARY KEY (`drugId`,`profileId`),
   ADD KEY `profileId` (`profileId`);
 
---
--- Indexes for table `profile`
---
 ALTER TABLE `profile`
   ADD PRIMARY KEY (`id`),
   ADD KEY `roleId` (`roleId`);
 
---
--- Indexes for table `role`
---
 ALTER TABLE `role`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `weightregistration`
---
 ALTER TABLE `weightregistration`
   ADD PRIMARY KEY (`id`),
   ADD KEY `weightregistrationProfileId` (`profileId`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `drug`
---
 ALTER TABLE `drug`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT for table `profile`
---
 ALTER TABLE `profile`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `role`
---
 ALTER TABLE `role`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `weightregistration`
---
 ALTER TABLE `weightregistration`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `drugprescription`
---
 ALTER TABLE `drugprescription`
   ADD CONSTRAINT `drugId` FOREIGN KEY (`drugId`) REFERENCES `drug` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `profileId` FOREIGN KEY (`profileId`) REFERENCES `profile` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `profile`
---
 ALTER TABLE `profile`
   ADD CONSTRAINT `roleId` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `weightregistration`
---
 ALTER TABLE `weightregistration`
   ADD CONSTRAINT `weightregistrationProfileId` FOREIGN KEY (`profileId`) REFERENCES `profile` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
